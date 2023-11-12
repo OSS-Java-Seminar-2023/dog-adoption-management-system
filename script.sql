68d93f6ebf9cb07fc2dea9329204c8b7673a2b0e
dea-bocina <146820239+dea-bocina@users.noreply.github.com>
2023-11-12 22:41:33 +0100

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,84 +1,78 @@
-CREATE TYPE role_enum AS ENUM ('User', 'Staff', 'Admin');
-CREATE TYPE gender_enum AS ENUM ('Male', 'Female');
-CREATE TYPE jobs_enum AS ENUM ('Walking & Exercise', 'Cleaning & Maintenance', 'Temporary Adoption', 'Feeding');
-CREATE TYPE adoption_status_enum AS ENUM ('Adopted', 'Not Adopted');
-CREATE TYPE status_enum AS ENUM ('Open', 'In Progress', 'Pending', 'Confirmed', 'Denied');
-CREATE TYPE no_yes_enum AS ENUM ('No', 'Yes');
-
 CREATE TABLE Users (
-    ID UNIQUEIDENTIFIER NOT NULL,
-    OIB BIGINT NOT NULL,
-    Role role_enum,
-    Username VARCHAR(20) NOT NULL,
-    Email NCHAR(40) NOT NULL,
-    Password NCHAR(20) NOT NULL,
-    Phone_Number VARCHAR(15) NOT NULL,
-    Name VARCHAR(20) NOT NULL,
-    Surname VARCHAR(20) NOT NULL,
-    Gender gender_enum,
-    Date_Of_Birth DATE NOT NULL,
-    City VARCHAR(20) NOT NULL,
-    Address VARCHAR(30) NOT NULL,
-    PRIMARY KEY (ID)
+                       id VARCHAR(36)  NOT NULL,
+                       oib BIGINT NOT NULL,
+                       role VARCHAR(20) NOT NULL,
+                       username VARCHAR(20) NOT NULL,
+                       email NCHAR(40) NOT NULL,
+                       password NCHAR(20) NOT NULL,
+                       phone_number VARCHAR(15) NOT NULL,
+                       name VARCHAR(20) NOT NULL,
+                       surname VARCHAR(20) NOT NULL,
+                       gender VARCHAR(10) NOT NULL,
+                       date_of_birth DATE NOT NULL,
+                       city VARCHAR(20) NOT NULL,
+                       address VARCHAR(30) NOT NULL,
+                       PRIMARY KEY (id)
 );
 
 CREATE TABLE Dogs (
-    ID UNIQUEIDENTIFIER NOT NULL,
-    Microchip BIGINT NOT NULL,
-    Name VARCHAR(20) NOT NULL,
-    Gender gender_enum,
-    Date_Of_Birth DATE NOT NULL,
-    Breed VARCHAR(20) NOT NULL,
-    Size INT NOT NULL,
-    Sterilised no_yes_enum,
-    Castrated no_yes_enum,
-    Note VARCHAR(50),
-    Adoption_Status adoption_status_enum,
-    Image VARCHAR(120) NOT NULL,
-    PRIMARY KEY (ID)
+                      id VARCHAR(36) NOT NULL,
+                      microchip BIGINT NOT NULL,
+                      name VARCHAR(20) NOT NULL,
+                      gender VARCHAR(10) NOT NULL,
+                      date_of_birth DATE NOT NULL,
+                      breed VARCHAR(30) NOT NULL,
+                      size VARCHAR(15) NOT NULL,
+                      sterilised VARCHAR(5) NOT NULL,
+                      castrated VARCHAR(5) NOT NULL,
+                      note VARCHAR(200),
+                      adoption_status VARCHAR(20) NOT NULL,
+                      image VARCHAR(120) NOT NULL,
+                      PRIMARY KEY (id)
 );
 
 CREATE TABLE Vaccines (
-    Vaccine_Name VARCHAR(25) NOT NULL,
-    PRIMARY KEY (Vaccine_Name)
+                      vaccine_name VARCHAR(25) NOT NULL,
+                      PRIMARY KEY (vaccine_name)
 );
 
 CREATE TABLE Dog_Vaccine (
-    Dog_ID UNIQUEIDENTIFIER NOT NULL,
-    Vaccine_Name VARCHAR(25) NOT NULL,
-    PRIMARY KEY (Vaccine_Name, Dog_ID),
-    FOREIGN KEY (Dog_ID) REFERENCES Dogs(ID),
-    FOREIGN KEY (Vaccine_Name) REFERENCES Vaccines(Vaccine_Name)
+                       dog_id VARCHAR(36) NOT NULL,
+                       vaccine_name VARCHAR(25) NOT NULL,
+                       PRIMARY KEY (vaccine_name, dog_id),
+                       FOREIGN KEY (dog_id) REFERENCES Dogs(id),
+                       FOREIGN KEY (vaccine_name) REFERENCES Vaccines(vaccine_name)
 );
 
 CREATE TABLE Volunteers (
-    ID UNIQUEIDENTIFIER NOT NULL,
-    User_ID UNIQUEIDENTIFIER NOT NULL,
-    Dog_ID UNIQUEIDENTIFIER NOT NULL,
-    Job jobs_enum,
-    Start_Time DATETIME NOT NULL,
-    Stop_Time DATETIME NOT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (User_ID) REFERENCES Users(ID),
-    FOREIGN KEY (Dog_ID) REFERENCES Dogs(ID)
+                        id VARCHAR(36) NOT NULL,
+                        user_id VARCHAR(36) NOT NULL,
+                        dog_id VARCHAR(36) NOT NULL,
+                        job VARCHAR(30) NOT NULL,
+                        start_time DATETIME NOT NULL,
+                        stop_time DATETIME NOT NULL,
+                        PRIMARY KEY (id),
+                        FOREIGN KEY (user_id) REFERENCES Users(id),
+                        FOREIGN KEY (dog_id) REFERENCES Dogs(id)
 );
 
 CREATE TABLE Waiting_List (
-    User_ID UNIQUEIDENTIFIER NOT NULL,
-    Dog_ID UNIQUEIDENTIFIER NOT NULL,
-    Date_Of_Application DATE NOT NULL,
-    PRIMARY KEY (User_ID, Dog_ID),
-    FOREIGN KEY (User_ID) REFERENCES Users(ID),
-    FOREIGN KEY (Dog_ID) REFERENCES Dogs(ID)
+                         id VARCHAR(36) NOT NULL,
+                         user_id VARCHAR(36) NOT NULL,
+                         dog_id VARCHAR(36) NOT NULL,
+                         date_of_application DATE NOT NULL,
+                         PRIMARY KEY (id),
+                         FOREIGN KEY (user_id) REFERENCES Users(id),
+                         FOREIGN KEY (dog_id) REFERENCES Dogs(id)
 );
 
 CREATE TABLE Contract (
-    ID UNIQUEIDENTIFIER NOT NULL,
-    User_ID UNIQUEIDENTIFIER NOT NULL,
-    Dog_ID UNIQUEIDENTIFIER NULL,
-    Date_Of_Contract DATE NOT NULL,
-    Status status_enum,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (User_ID) REFERENCES Users(ID),
-    FOREIGN KEY (Dog_ID) REFERENCES Dogs(ID)
+                          id VARCHAR(36) NOT NULL,
+                          user_id VARCHAR(36) NOT NULL,
+                          dog_id VARCHAR(36) NULL,
+                          date_of_contract DATE NOT NULL,
+                          status VARCHAR(20) NOT NULL,
+                          PRIMARY KEY (id),
+                          FOREIGN KEY (user_id) REFERENCES Users(id),
+                          FOREIGN KEY (dog_id) REFERENCES Dogs(id)
 );
