fa8309feb65c97e993d766c7d936bb88556cd189
dea-bocina <146820239+dea-bocina@users.noreply.github.com>
2023-11-20 16:01:33 +0100

    Delete test directory

--- test/DogAdoptionApplicationTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class DogAdoptionApplicationTests {
-    @Test
-    public void contextLoads() {
-    }
-}
