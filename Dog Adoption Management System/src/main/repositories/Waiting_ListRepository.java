0956052213c36b129d355be5e106992815b43669
Josip-Grljusic <146939280+Josip-Grljusic@users.noreply.github.com>
2023-12-04 15:05:42 +0100

    Delete Dog Adoption Management System directory

--- Dog Adoption Management System/src/main/repositories/Waiting_ListRepository.java
+++
@@ -1,9 +0,0 @@
-package main.repositories;
-import main.entities.Waiting_List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.UUID;
-
-@Repository
-public interface Waiting_ListRepository extends JpaRepository<Waiting_List, UUID> {
-}
