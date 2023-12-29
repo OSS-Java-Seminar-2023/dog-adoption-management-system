d2b59ec191143447bda36ec44ec48a4c150cf00d
dea-bocina <146820239+dea-bocina@users.noreply.github.com>
2023-12-30 00:08:29 +0100

    Delete DogAdoptionManagementSystem-main/src/main directory

--- DogAdoptionManagementSystem-main/src/main/java/com/dog/adoption/controllers/ContractController.java
+++
@@ -1,8 +0,0 @@
-package com.dog.adoption.controllers;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-
-@Controller
-@AllArgsConstructor
-public class ContractController {
-}
--- DogAdoptionManagementSystem-main/src/main/java/com/dog/adoption/repository/DogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dog.adoption.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.dog.adoption.models.Dog;
-import org.springframework.stereotype.Repository;
-import java.util.UUID;
-@Repository
-public interface DogRepository extends JpaRepository<Dog, UUID>{
-}
