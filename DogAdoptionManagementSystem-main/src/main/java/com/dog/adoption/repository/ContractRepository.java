ae35a5d0350f04aafe8acefb9fcadc53d157f456
dea-bocina <146820239+dea-bocina@users.noreply.github.com>
2024-01-02 00:38:55 +0100

    Delete DogAdoptionManagementSystem-main/src/main directory

--- DogAdoptionManagementSystem-main/src/main/java/com/dog/adoption/repository/ContractRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dog.adoption.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.dog.adoption.models.Contract;
-import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
-@Repository
-public interface ContractRepository extends JpaRepository<Contract, UUID> {
-}
