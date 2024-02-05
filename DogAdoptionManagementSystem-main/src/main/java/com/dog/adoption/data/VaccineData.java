c772332cf70b3505f32b94cac6bc579a1038db26
dea-bocina <146820239+dea-bocina@users.noreply.github.com>
2024-02-05 12:37:17 +0100

    Delete DogAdoptionManagementSystem-main/src/main directory

--- DogAdoptionManagementSystem-main/src/main/java/com/dog/adoption/data/VaccineData.java
+++
@@ -1,6 +0,0 @@
-package com.dog.adoption.data;
-import lombok.Data;
-@Data
-public class VaccineData {
-    private String vaccineName;
-}
--- DogAdoptionManagementSystem-main/src/main/java/com/dog/adoption/services/VaccineService.java
+++
@@ -1,9 +0,0 @@
-package com.dog.adoption.services;
-import lombok.AllArgsConstructor;
-import com.dog.adoption.models.Vaccine;
-import org.springframework.stereotype.Service;
-import java.util.List;
-@Service
-public interface VaccineService {
-    List<Vaccine> listAllVaccines();
-}
